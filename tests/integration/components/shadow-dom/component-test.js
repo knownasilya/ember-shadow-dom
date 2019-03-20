3a25ed8f277d2ca82b347f7a78f5015464d67cb0
Ilya Radchenko <knownasilya@gmail.com>
2019-03-19 20:15:09 -0400

    fix: mode arg and add closed test

--- tests/integration/components/shadow-dom/component-test.js
+++ tests/integration/components/shadow-dom/component-test.js
@@ -8,11 +8,21 @@ module('Integration | Component | shadow-dom', function(hooks) {
 
   test('it renders', async function(assert) {
     await render(hbs`
-      <ShadowDom id='internal'>
+      <ShadowDom id='internal' @mode='open'>
         <div class='block'>template block text</div>
       </ShadowDom>
     `);
 
     assert.dom('.block', find('#internal').shadowRoot).hasText('template block text');
+  });
+
+  test('it can be closed', async function(assert) {
+    await render(hbs`
+      <ShadowDom id='internal' @mode='closed'>
+        <div class='block'>template block text</div>
+      </ShadowDom>
+    `);
+
+    assert.dom('.block', find('#internal').shadowRoot).doesNotExist();
   });
 });
