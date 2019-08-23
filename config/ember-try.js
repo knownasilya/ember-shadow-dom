3eb6d623b79c528e11fe048731a37b26fdef8133
Ilya Radchenko <knownasilya@gmail.com>
2019-08-23 10:42:37 -0400

    fix: add back custom tagname

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,22 +11,10 @@ module.exports = function() {
     return {
       scenarios: [
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
+          name: 'ember-lts-3.7.2',
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-3.4',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.4.0'
+              'ember-source': '~3.7.2'
             }
           }
         },
