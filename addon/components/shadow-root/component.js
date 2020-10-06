3b10e7cac16c1c5f9e9706b3c3af10b852a8b9dd
Ilya Radchenko <knownasilya@gmail.com>
2020-10-05 20:14:15 -0400

    fix: attempt fixing double set

--- addon/components/shadow-root/component.js
+++ addon/components/shadow-root/component.js
@@ -5,15 +5,15 @@ import { action } from '@ember/object';
 const isFastBoot = typeof FastBoot !== 'undefined';
 
 export default class ShadowRoot extends Component {
-  @tracked shadow = null;
-  @tracked tagName = 'div';
+  @tracked shadow;
+  @tracked tagName;
 
   defaultMode = 'open';
   isFastBoot = isFastBoot;
 
   constructor() {
     super(...arguments);
-    this.tagName = this.args.tagName || this.tagName;
+    this.tagName = this.args.tagName || 'div';
   }
 
   @action
