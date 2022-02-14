0af04bbf2d27c61a6a6d38909a3226ac74a351f7
Ilya Radchenko <knownasilya@gmail.com>
2022-02-14 08:06:39 -0500

    chore: try to fix classic test

--- tests/integration/components/shadow-root/component-test.js
+++ tests/integration/components/shadow-root/component-test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { find, render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
+import { macroCondition, dependencySatisfies } from '@embroider/macros';
 
 module('Integration | Component | shadow-root', function (hooks) {
   setupRenderingTest(hooks);
@@ -13,7 +14,17 @@ module('Integration | Component | shadow-root', function (hooks) {
       </ShadowRoot>
     `);
 
-    assert.dom('.block', find('div').shadowRoot).hasText('template block text');
+    // Ember classic that uses template-only-glimmer-components
+    // No way to check if that feature is enabled https://github.com/pzuraq/ember-compatibility-helpers/issues/27
+    if (macroCondition(dependencySatisfies('ember-source', '<=3.24.0'))) {
+      assert
+        .dom('.block', find('div > div').shadowRoot)
+        .hasText('template block text');
+    } else {
+      assert
+        .dom('.block', find('div').shadowRoot)
+        .hasText('template block text');
+    }
   });
 
   test('it can be closed', async function (assert) {
